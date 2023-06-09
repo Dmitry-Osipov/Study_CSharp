72d3bf9c7f05cacb27bebcd09943af48871326df
Dmitry Osipov <dimaosipov00@gmail.com>
2023-06-09 18:41:12 +0300

    added dir seminar and homework. Complete homework on C#

---
+++ homework/homework_C#_1/Program.cs
@@ -0,0 +1,104 @@
+﻿//Задача 1: Напишите программу, которая на вход принимает два числа и выдаёт,
+// какое число большее, а какое меньшее.
+//a = 5; b = 7 -> max = 7
+//a = 2 b = 10 -> max = 10
+//a = -9 b = -3 -> max = -3
+
+Console.WriteLine("Task: find max number");
+Console.Write("Input number_1: ");
+int number_1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Input number_2: ");
+int number_2 = Convert.ToInt32(Console.ReadLine());
+if (number_1 > number_2)
+{
+    Console.WriteLine($"max = {number_1}");
+}
+else
+{
+    Console.WriteLine($"max = {number_2}");
+}
+
+//Задача 2: Напишите программу, которая принимает на вход три числа и
+// выдаёт максимальное из этих чисел.
+//2, 3, 7 -> 7
+//44 5 78 -> 78
+//22 3 9 -> 22
+
+Console.WriteLine("Task: find max number of 3 numbers");
+Console.Write("Input number_1: ");
+int new_number_1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Input number_2: ");
+int new_number_2 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Input number_3: ");
+int new_number_3 = Convert.ToInt32(Console.ReadLine());
+if (new_number_1 > new_number_2 && new_number_1 > new_number_3)
+{
+    Console.WriteLine($"max = {new_number_1}");
+}
+if (new_number_2 > new_number_1 && new_number_2 > new_number_3)
+{
+    Console.WriteLine($"max = {new_number_2}");
+}
+if (new_number_3 > new_number_1 && new_number_3 > new_number_2)
+{
+    Console.WriteLine($"max = {new_number_3}");
+}
+
+//Задача 3: Напишите программу, которая на вход принимает число и выдаёт,
+// является ли число чётным (делится ли оно на два без остатка).
+//4 -> да
+//-3 -> нет
+//7 -> нет
+
+Console.WriteLine("Task: find even or odd number");
+Console.Write("Input number: ");
+int user_number = Convert.ToInt32(Console.ReadLine());
+if (user_number % 2 == 0)
+{
+    Console.WriteLine("Your number is even");
+}
+else
+{
+    Console.WriteLine("Your number is odd");
+}
+
+//Задача 4: Напишите программу, которая на вход принимает число (N),
+// а на выходе показывает все чётные числа от 1 до N.
+//5 -> 2, 4
+//8 -> 2, 4, 6, 8
+
+Console.WriteLine("Task: output all even numbers in count of user_number");
+Console.Write("Input number: ");
+int new_user_number = Convert.ToInt32(Console.ReadLine());
+int count = 1;
+if (new_user_number >= 2)
+{
+while (count <= new_user_number)
+    {
+        if (count % 2 == 0)
+        {
+            Console.WriteLine(count);
+            count += 1;
+        }
+        else
+        {
+            count += 1;
+        }
+    }
+}
+else
+if (new_user_number < 2)
+{
+    while (count >= new_user_number)
+    {
+        if (count % 2 == 0)
+        {
+            Console.WriteLine(count);
+            count -= 1;
+        }
+        else
+        {
+            count -= 1;
+        }
+    }
+}
---
+++ seminar/seminar1/Program.cs
@@ -0,0 +1,75 @@
+﻿/*
+Console.WriteLine("Today is Friday");
+Console.Write("Today is Friday");
+*/
+// /**/ - закомментировать часть кода
+
+
+//Console.ReadLine("Input : "); // str
+/*
+Console.Write("Input number: ");
+int user_number = Convert.ToInt32(Console.ReadLine()); // int
+Console.WriteLine("Your number is - " + user_number);
+Console.WriteLine($"Your number is - {user_number}");
+*/
+
+/*
+// Задача 1:
+// Напишите прошрамму, которая принимает число и выдаёт его квадрат
+// 1. Узнайте у пользователя число
+// 2. Вводим в квадрат
+// 3. Выводим результат
+Console.Write("Input number: "); // 1 шаг
+int user_number = Convert.ToInt32(Console.ReadLine()); // 1 шаг
+int user_number_sqr = user_number * user_number; // 2 шаг
+Console.WriteLine(user_number_sqr); // 3 шаг
+*/
+
+/*
+// Задача 2: 
+// Напишите программу, которая на вход принимает два числа и проверяет, 
+// является ли первое число квадратом второго.
+Console.Write("Input number 1: ");
+int number_1 = Convert.ToInt32(Console.ReadLine()); 
+Console.Write("Input number 2: ");
+int number_2 = Convert.ToInt32(Console.ReadLine()); 
+if (number_2 == number_1 * number_1)
+{
+    Console.WriteLine($"{number_2} IS square of {number_1}");
+}
+else
+{
+    Console.WriteLine($"{number_2} IS NOT square of {number_1}");
+}
+*/
+
+/*
+// Задача 3:
+// Напишите программу, которая на вход принимает одно число (N), 
+// а на выходе показывает все целые числа в промежутке от -N до N.
+Console.Write("Input positive number: ");
+int user_number = Convert.ToInt32(Console.ReadLine());
+int another_user_number = user_number * -1;
+int count = another_user_number;
+while (count <= user_number)
+{
+    Console.WriteLine(count);
+    count += 1;
+}
+*/
+
+
+// Задача 4:
+// Напишите программу, которая принимает на вход трёхзначное число и
+// на выходе показывает последнюю цифру этого числа.
+Console.Write("Input three-digit number: ");
+int user_number = Convert.ToInt32(Console.ReadLine());
+if (user_number > 99 && user_number < 1000)
+{
+    int last_number = user_number % 10;
+    Console.WriteLine($"Last number of {user_number} is {last_number}");
+}
+else
+{
+    Console.WriteLine("Yout number is not three-digit number");
+}
