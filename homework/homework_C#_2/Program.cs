f2637da202c94daa82194e5fa016e5c417695561
Dmitry Osipov <dimaosipov00@gmail.com>
2023-06-13 16:33:48 +0300

    update seminar and homework

---
+++ homework/homework_C#_2/Program.cs
@@ -0,0 +1,107 @@
+﻿// Задача 1: Напишите программу, которая принимает на вход трёхзначное число и 
+// на выходе показывает вторую цифру этого числа.
+// 456 -> 5
+// 782 -> 8
+// 918 -> 1
+
+void find_second_digit()
+{
+    Console.Write("Input three digit number: ");
+    int user_number = Convert.ToInt32(Console.ReadLine());
+    if (user_number > 99 && user_number < 1000)
+    {
+        int two_digit = user_number % 100;
+        int second_digit = two_digit / 10;
+        Console.WriteLine($"The second digit of the number is {second_digit}");
+    }
+    else
+    {
+        Console.WriteLine("Your number is not three-digit");
+    }
+}
+find_second_digit();
+
+// Задача 2: Напишите программу, которая выводит третью цифру заданного числа или сообщает, 
+// что третьей цифры нет.
+// 645 -> 5
+// 78 -> третьей цифры нет
+// 32679 -> 6
+
+// Способ для конкретных разрядов:
+void find_third_digit()
+{
+    int random_number = new Random().Next(0, 100000);
+//  для проверки:
+//    int random_number = 34;
+//    int random_number = 345;
+//    int random_number = 3456;
+//    int random_number = 34567;
+    Console.WriteLine($"Number is {random_number}");
+    if (random_number < 100)
+    {
+        Console.WriteLine("The number does not have a third digit");
+    }
+    else if (random_number >= 100 && random_number < 1000)
+    {
+        int digit_of_3 = random_number % 10;
+        Console.WriteLine($"The third digit of the number is {digit_of_3}");
+    }
+    else if (random_number >= 1000 && random_number <= 10000)
+    {
+        int digit_of_4 = random_number / 10;
+        int digit_of_3 = digit_of_4 % 10;
+        Console.WriteLine($"The third digit of the number is {digit_of_3}");
+    }
+    else if (random_number >= 10000 && random_number < 100000)
+    {
+        int digit_of_5 = random_number / 100;
+        int digit_of_3 = digit_of_5 % 10;
+        Console.WriteLine($"The third digit of the number is {digit_of_3}");
+    }
+}
+find_third_digit();
+
+// Универсальный способ через приведение к строке
+void FIND_THIRD_DIGIT()
+{
+    int RANDOM_NUMBER = new Random().Next(0, 1000000000);
+    Console.WriteLine($"Number is {RANDOM_NUMBER}");
+    if (RANDOM_NUMBER < 100)
+    {
+        Console.WriteLine("The number does not have a third digit");
+    }
+    else
+    {
+        string THIRD_DIGIT = RANDOM_NUMBER.ToString();
+        Console.WriteLine($"The third digit of the number is {THIRD_DIGIT[2]}");
+    }
+}
+FIND_THIRD_DIGIT();
+
+// Задача 3: Напишите программу, которая принимает на вход цифру, обозначающую день недели, 
+// и проверяет, является ли этот день выходным.
+// 6 -> да
+// 7 -> да
+// 1 -> нет
+
+void week()
+{
+    Console.Write("Input the day of the week in numerical format: ");
+    int number = Convert.ToInt32(Console.ReadLine());
+    if (number > 0 && number < 8)
+    {
+        if (number == 6 || number == 7)
+        {
+            Console.WriteLine("This day is a day off");
+        }
+        else
+        {
+            Console.WriteLine("This day is a weekday");
+        }
+    }
+    else
+    {
+        Console.WriteLine("Enter the correct number");
+    }
+}
+week();
---
+++ seminar/seminar2/Program.cs
@@ -0,0 +1,101 @@
+﻿// Напишите программу, которая выводит случайное число из отрезка [10, 99] и показывает наибольшую цифру
+
+int Line(){
+    int num = new Random().Next(10, 100);
+    Console.WriteLine("Your num is " + num);
+    int dec = num / 10;
+    int ed = num % 10;
+    if (dec > ed){
+        return dec;
+    }
+    else{
+        return ed;
+    }
+}
+
+Console.WriteLine($"Result is {Line()}");
+
+
+// Напишите программу, которая выводит случайное трёхзначное число и удаляет вторую цифру этого числа.
+
+void Line(){
+    int num = new Random().Next(100, 1000);
+    Console.WriteLine("Your num is " + num);
+    int first = num / 100;
+    int third = num % 10;
+    Console.Write($"{first}{third}");
+}
+Line();
+
+// Напишите программу, которая будет принимать на вход два числа и выводить, 
+// является ли второе число кратным первому. Если число 2 не кратно числу 1, 
+// то программа выводит остаток от деления.
+
+void NUMBERS()
+{
+    Console.Write("Введите число 1: ");
+    int number_1 = Convert.ToInt32(Console.ReadLine());
+    Console.Write("Введите число 2: ");
+    int number_2 = Convert.ToInt32(Console.ReadLine());
+    if (number_2 % number_1 == 0)
+    {
+        Console.WriteLine("Второе число кратно первому");
+    }
+    else
+    {
+        Console.WriteLine(number_2 % number_1);
+    }
+}
+NUMBERS();
+
+
+// Напишите программу, которая принимает на вход число и проверяет, кратно ли оно одновременно
+// 7 и 23
+
+void crat()
+{
+    Console.Write("Введите число: ");
+    int number = Convert.ToInt32(Console.ReadLine());
+    if (number != 0)
+    {
+        if (number % 7 == 0 && number % 23 == 0)
+        {
+            Console.WriteLine("Число кратно 7 и 23 одновременно");
+        }
+        else
+        {
+            Console.WriteLine("Число НЕ кратно 7 и 23 одновременно");
+        }
+    }
+}
+crat();
+
+// Напишите программу, которая принимает на вход 2 числа и проверяет, 
+// является ли одно число квадратом другого
+
+void Numbers()
+{
+    Console.Write("Введите число 1: ");
+    int number1 = Convert.ToInt32(Console.ReadLine());
+    Console.Write("Введите число 2: ");
+    int number2 = Convert.ToInt32(Console.ReadLine());
+    if (number1 < 0)
+    {
+        number1 *= (-1);
+    }
+    else if (number2 < 0)
+    {
+        number2 *= (-1);
+    }
+    Console.WriteLine($"{number1}, {number2}");
+    if (number2 == number1 * number1 || number1 == number2 * number2)
+    {
+        Console.WriteLine("Одно число является квадратом другого числа");
+    }
+    else
+    {
+        Console.WriteLine("Ни одино число не является квадратом другого числа");
+    }
+}
+
+Numbers();
